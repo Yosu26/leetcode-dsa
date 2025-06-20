42bef58cc47e5bdfa08346f8072022d3fe714ba7
Yosu26 <yosu7013@gmail.com>
2025-06-20 23:29:40 +0800

    initial commit

---
+++ linked_list/003_swap_nodes_in_pair.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+// /**
+//  * Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+
+        ListNode* prev = dummy;
+        while(head && head->next){
+            ListNode* first = head;
+            ListNode* second = head->next;
+
+            //Swap
+            prev->next = second;
+            first->next = second->next;
+            second->next = first;
+
+            //Move forward 
+            prev = first;
+            head = first->next;
+        }
+        return dummy->next;
+    }
+};
