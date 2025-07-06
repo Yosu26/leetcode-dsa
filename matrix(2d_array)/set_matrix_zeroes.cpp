d8ceefa34859aa86e20f4faab758c5ff5f74b25c
Yosu26 <yosu7013@gmail.com>
2025-07-06 17:27:19 +0800

    initial commit

---
+++ matrix(2d_array)/set_matrix_zeroes.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int rows = matrix.size(), cols = matrix[0].size();
+        bool rowZero = false;
+
+        // Determine which cols/rows need to be zero
+        for(int r = 0; r < rows; ++r){
+            for(int c = 0; c < cols; ++c){
+                if(matrix[r][c] == 0){
+                    matrix[0][c] = 0;
+                    if(r > 0){
+                        matrix[r][0] = 0;
+                    } else {
+                        rowZero = true;
+                    }
+                }
+            }
+        }
+
+        for(int r = 1; r < rows; ++r){
+            for(int c = 1; c < cols; ++c){
+                if(matrix[0][c] == 0 || matrix[r][0] == 0){
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+
+        if(matrix[0][0] == 0){
+            for(int r = 0; r < rows; ++r){
+                matrix[r][0] = 0;
+            }
+        }
+
+        if(rowZero){
+            for(int c = 0; c < cols; ++c){
+                matrix[0][c] = 0;
+            }
+        }
+    }
+};
