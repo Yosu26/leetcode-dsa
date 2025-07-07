20659b5ba79174d8418f4b53e3f006200f60480f
Yosu26 <yosu7013@gmail.com>
2025-07-07 18:04:22 +0800

    initial commit

---
+++ stacks/valid_parentheses.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+
+        for(char c : s){
+            if(c == '(' || c == '{' || c == '['){
+                stk.push(c);
+            } else{
+                if(stk.empty()) return false;
+                
+                char top = stk.top();
+                if((c == ')' && top != '(' ||
+                    c == '}' && top != '{' ||
+                    c == ']' && top != '[')) {
+                    return false;
+                }
+
+                stk.pop();
+            }
+        }
+        return stk.empty();
+    }
+};
